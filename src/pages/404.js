ec4c11d796fc7655f03075edbbb5f9da1eb7ef67
Sri Prasad Tadimalla <prasad_tad@yahoo.com>
2017-10-25 20:07:12 +1100

    Using gatsby-link

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import Link from 'gatsby-link'
 
 const NotFoundPage = () => (
   <div>
     <h1>Oh no, you are lost!</h1>
     <h2>Try the following links instead:</h2>
     <ul>
-      <li><a href="/">Home</a></li>
-      <li><a href="/collections/">Collections</a></li>
-      <li><a href="/cuisines/">Cuisines</a></li>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/collections/">Collections</Link></li>
+      <li><Link to="/cuisines/">Cuisines</Link></li>
     </ul>
   </div>
 )
